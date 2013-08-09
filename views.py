ffa80550577de25f5b4963d4d647c2d29c25130f
thequbit <thequbit@gmail.com>
2013-08-09 13:12:48 -0400

    removed punctuation from tweet text, and added 'black word' list

--- views.py
+++ views.py
@@ -13,12 +13,15 @@ def words_view(request):
     text = "".join([i for i in text if ord(i) in range(32, 127)])
     text = re.sub('http://','',text)
     text = re.sub(' +',' ',text)
+    text = text.replace(',','').replace('.','').replace('!','').replace('?','')
+    text = text.replace(':','').replace(';','').replace("'",'').replace("\"",'')
     text = text.lower()
     _tokens = nltk.word_tokenize(text)
     tokens = nltk.FreqDist(word.lower() for word in _tokens)
+    ignorewords= ['and','but','its','for','roc','the','you','out','via']
     words = []
     for token,frequency in tokens.items():
-        if len(token) > 3:
+        if len(token) > 2 and not token in ignorewords:
             word = []
             word.append(token)
             word.append(frequency)
